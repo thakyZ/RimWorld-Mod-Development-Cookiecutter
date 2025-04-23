3f77275aa0a840d85a452fc7c795b89e93524f02
thakyZ <950594+thakyZ@users.noreply.github.com>
2025-04-23 07:21:39 -0700

    fix: spelling of mod_name

--- RimWorld-{{cookiecutter.mod_name.replace(' ', '_')}}/Source/{{cookiecutter.mod_name.replace('-','_').replace(' ','')}}Mod.cs
+++ RimWorld-{{cookiecutter.mod_name.replace(' ', '_')}}/Source/{{cookiecutter.mod_name.replace('-','_').replace(' ','')}}Mod.cs
@@ -4,7 +4,7 @@ using Verse;
 using RimWorld;
 using HarmonyLib;
 namespace {{ cookiecutter.mod_name.replace('-','_').replace(' ','') }};
-public class {{ cookiecutter.mod_nam.replace('-','_').replace(' ','') }}Mod : Mod
+public class {{ cookiecutter.mod_name.replace('-','_').replace(' ','') }}Mod : Mod
 {
     public static readonly {{ cookiecutter.mod_name.replace('-','_').replace(' ','') }}Settings settings;
     private static readonly Harmony harmony;
@@ -28,7 +28,7 @@ public class {{ cookiecutter.mod_nam.replace('-','_').replace(' ','') }}Mod : Mo
     public override string SettingsCategory() => "{{ cookiecutter.mod_name.replace('-','_').replace(' ','') }}.Settings.Title".Translate();
 }
 
-public class {{ cookiecutter.mod_nam.replace('-','_').replace(' ','') }}Settings : ModSettings
+public class {{ cookiecutter.mod_name.replace('-','_').replace(' ','') }}Settings : ModSettings
 {
     public bool debugLog = false;
     public override void ExposeData()
